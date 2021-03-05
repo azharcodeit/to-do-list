5256d3228493642d98e9ecfb0427a5affd43890d
azharcodeit <azhar.botbayeva@gmail.com>
2021-03-05 20:14:29 +0600

    feat: added data attributes to buttons

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
 'use strict';
 
-const btnAdd = document.querySelector('.btn--add');
-const btnCompleted = document.querySelector('.btn--done');
-const btnEmpty = document.querySelector('.btn--all');
+const mainContainer = document.getElementById('main');
 let table = document.querySelector('.items');
 let taskFilter = document.querySelector('.filter');
 const modal = document.querySelector('.modal');
@@ -19,67 +17,23 @@ const btnEdit = document.querySelector('.modal--edit');
 const btnDelete = document.querySelector('.modal--delete');
 const editTitle = document.querySelector('.title--edit');
 const editContext = document.querySelector('.context--edit');
+const TodoTableCellIndex = {
+    OrderCell: 0,
+    TaskCell: 1,
+    ContextCell: 2,
+    EditCell: 3,
+    DeleteCell: 4,
+};
 
 
-btnModalAdd.addEventListener('click', function () {
+mainContainer.addEventListener('dblclick', completeTask);
+mainContainer.addEventListener('click', deleteCompleted);
+mainContainer.addEventListener('click', deleteAll);
+mainContainer.addEventListener('click', addTaskModal);
+modal.addEventListener('click', addTask);
+mainContainer.addEventListener('click', deleteTaskModal);
+mainContainer.addEventListener('click', editTaskModal);
 
-    if (taskTitle.value.length !== 0) {
-        let row = table.insertRow(-1);
-        let orderCell = row.insertCell(0);
-        let taskCell = row.insertCell(1);
-        let contextCell = row.insertCell(2);
-        let editCell = row.insertCell(3);
-        let deleteCell = row.insertCell(4);
-
-        row.addEventListener("dblclick", clickHandler);
-        orderCell.classList.add('nr');
-        deleteCell.classList.add('nr');
-        editCell.classList.add('nr');
-
-        for (let i = 0; table.rows[i]; i++) { orderCell.innerHTML = i + 1; };
-        taskCell.innerHTML = taskTitle.value;
-        contextCell.innerHTML = taskContext.value;
-        taskTitle.value = '';
-        taskContext.value = '';
-
-        let imgEdit = document.createElement('img');
-        imgEdit.src = "/icons/edit.png";
-        imgEdit.onmouseover = function () { imgEdit.src = "/icons/edit-hover.png"; };
-        imgEdit.onmouseout = function () { imgEdit.src = "/icons/edit.png"; };
-
-        editHandler();
-
-        let imgDelete = document.createElement('img');
-        imgDelete.src = "/icons/delete.png";
-        imgDelete.onmouseover = function () { imgDelete.src = "/icons/delete-hover.png"; };
-        imgDelete.onmouseout = function () { imgDelete.src = "/icons/delete.png"; };
-
-        deleteHandler();
-
-        deleteCell.appendChild(imgDelete);
-        editCell.appendChild(imgEdit);
-
-    } else { alert('Task is empty') };
-
-});
-
-btnCompleted.addEventListener('click', function () {
-    for (let i = table.rows.length - 1; i >= 0; i--) {
-        if (table.rows[i].classList.contains('completed-task')) {
-            table.deleteRow(i);
-        };
-    };
-    for (let i = 0; table.rows[i]; i++) {
-        table.rows[i].cells[0].innerHTML = i + 1;
-    };
-
-});
-
-btnEmpty.addEventListener('click', function () {
-    for (let i = table.rows.length - 1; i >= 0; i--) {
-        table.deleteRow(i);
-    }
-});
 
 taskFilter.addEventListener('keyup', function () {
     let filter, i, txtValue;
@@ -93,84 +47,165 @@ taskFilter.addEventListener('keyup', function () {
         }
     };
 });
-function clickHandler() {
-    this.classList.toggle('completed-task');
+
+function completeTask(event) {
+    let tr = event.target.closest('tr');
+    if (!tr) return;
+    if (!table.contains(tr)) return;
+    tr.classList.toggle('completed-task');
+};
+
+function deleteCompleted(event) {
+    if (event.target.getAttribute("data-action") === "deleteCompleted") {
+        for (let i = table.rows.length - 1; i >= 0; i--) {
+            if (table.rows[i].classList.contains('completed-task')) {
+                table.deleteRow(i);
+            };
+        };
+        for (let i = 0; table.rows[i]; i++) {
+            table.rows[i].cells[TodoTableCellIndex.OrderCell].innerHTML = i + 1;
+        };
+    }
+};
+
+function deleteAll(event) {
+    if (event.target.getAttribute("data-action") === "deleteAll") {
+        for (let i = table.rows.length - 1; i >= 0; i--) {
+            table.deleteRow(i);
+        }
+    };
+};
+
+function addTaskModal(event) {
+    if (event.target.getAttribute("data-action") !== "addTask") return;
+    openModal();
+};
+
+function addTask(event) {
+    if (event.target.getAttribute("data-action") !== "add") return;
+    if (taskTitle.value.length === 0) return;
+    let row = table.insertRow(-1);
+    let orderCell = row.insertCell(TodoTableCellIndex.OrderCell);
+    let taskCell = row.insertCell(TodoTableCellIndex.TaskCell);
+    let contextCell = row.insertCell(TodoTableCellIndex.ContextCell);
+    let editCell = row.insertCell(TodoTableCellIndex.EditCell);
+    let deleteCell = row.insertCell(TodoTableCellIndex.DeleteCell);
+
+    orderCell.classList.add('nr');
+    deleteCell.classList.add('nr');
+    editCell.classList.add('nr');
+
+    orderCell.innerHTML = table.rows.length;
+    taskCell.innerHTML = taskTitle.value;
+    contextCell.innerHTML = taskContext.value;
+    taskTitle.value = taskContext.value = '';
+
+    let imgEdit = document.createElement('img');
+    imgEdit.src = "./icons/edit.png";
+    imgEdit.setAttribute("data-action", "editRow");
+
+    imgEdit.onmouseover = function () {
+        imgEdit.src = "./icons/edit-hover.png";
+    };
+
+    imgEdit.onmouseout = function () {
+        imgEdit.src = "./icons/edit.png";
+    };
+
+    let imgDelete = document.createElement('img');
+    imgDelete.src = "./icons/delete.png";
+    imgDelete.setAttribute("data-action", "deleteRow");
+
+    imgDelete.onmouseover = function () {
+        imgDelete.src = "./icons/delete-hover.png";
+    };
+
+    imgDelete.onmouseout = function () {
+        imgDelete.src = "./icons/delete.png";
+    };
+
+    deleteCell.appendChild(imgDelete);
+    editCell.appendChild(imgEdit);
+    editCell.setAttribute("data-action", "editRow");
+    deleteCell.setAttribute("data-action", "deleteRow");
 
 };
 
-const openModal = function () {
+function deleteTaskModal(event) {
+    if (event.target.getAttribute("data-action") === "deleteRow") {
+        let tr = event.target.closest('tr');
+        openModalDelete();
+        deleteTask(tr);
+    };
+};
+
+function deleteTask(index) {
+    btnDelete.addEventListener('click', function () {
+        index.remove();
+        for (let i = 0; table.rows[i]; i++) {
+            table.rows[i].cells[0].innerHTML = i + 1;
+        };
+        closeModal();
+    });
+};
+
+function editTaskModal(event) {
+    if (event.target.getAttribute("data-action") !== "editRow") return;
+    let tr = event.target.closest('tr');
+    openModalEdit();
+    editTask(tr);
+};
+
+function editTask(index) {
+    editTitle.value = index.cells[TodoTableCellIndex.TaskCell].innerHTML;
+    editContext.innerHTML = index.cells[TodoTableCellIndex.ContextCell].innerHTML;
+    btnEdit.onclick = function () {
+        if (editTitle.value.length === 0) return;
+        index.cells[TodoTableCellIndex.TaskCell].innerHTML = editTitle.value;
+        index.cells[TodoTableCellIndex.ContextCell].innerHTML = editContext.value;
+        for (let i = 0; table.rows[i]; i++) {
+            table.rows[i].cells[TodoTableCellIndex.OrderCell].innerHTML = i + 1;
+        };
+        closeModal();
+    };
+};
+
+function openModal() {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
 
-const openModalDelete = function () {
+function openModalDelete() {
     modalDelete.classList.remove('hidden');
     overlay.classList.remove('hidden');
-};
-
-const closeModal = function () {
-    modalDelete.classList.add('hidden');
-    modalEdit.classList.add('hidden');
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-};
-
-btnAdd.addEventListener('click', openModal);
-for (let i = 0; i < btnCloseModal.length; i++) {
-    btnCloseModal[i].addEventListener('click', closeModal);
-};
-for (let i = 0; i < btnCancel.length; i++) {
-    btnCancel[i].addEventListener('click', closeModal);
-};
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) { // e for event/looking up to the object of an event; define function
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
-});
-
-const deleteHandler = function () {
-    for (let i = 0; i < table.rows.length; i++) {
-        table.rows[i].cells[4].onclick = function () {
-            let index = this.parentElement.rowIndex;
-            openModalDelete();
-            btnDelete.onclick = function () {
-                table.deleteRow(index);
-                for (let i = 0; table.rows[i]; i++) {
-                    table.rows[i].cells[0].innerHTML = i + 1;
-                };
-                closeModal();
-            };
-        };
-    };
-};
-
-const editHandler = function () {
-    for (let i = 0; i < table.rows.length; i++) {
-        table.rows[i].cells[3].onclick = function () {
-            let index = this.parentElement.rowIndex;
-            openModalEdit();
-            editTitle.value = table.rows[index].cells[1].innerHTML;
-            editContext.inne
-            rHTML = table.rows[index].cells[2].innerHTML;
-            btnEdit.onclick = function () {
-                if (editTitle.value.length !== 0) {
-                    table.rows[index].cells[1].innerHTML = editTitle.value;
-                    table.rows[index].cells[2].innerHTML = editContext.value;
-                    for (let i = 0; table.rows[i]; i++) {
-                        table.rows[i].cells[0].innerHTML = i + 1;
-                    };
-                }
-                else { alert('Title can not be empty!'); };
-                closeModal();
-
-            };
-        };
-    };
 };
 
 const openModalEdit = function () {
     modalEdit.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
+
+function closeModal() {
+    modalDelete.classList.add('hidden');
+    modalEdit.classList.add('hidden');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+overlay.addEventListener('click', closeModal);
+
+for (let i = 0; i < btnCloseModal.length; i++) {
+    btnCloseModal[i].addEventListener('click', closeModal);
+};
+for (let i = 0; i < btnCancel.length; i++) {
+    btnCancel[i].addEventListener('click', closeModal);
+};
+
+document.addEventListener('keydown', escClose);
+function escClose(e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+};
+
+
